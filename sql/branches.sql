3645dcdfb33278805f331946a4b5cec650f2d69a
ariana.gritsiuk <ariana.gritsiuk@localhost>
2026-05-25 19:59:52 +0400

    add shelter branches data

---
+++ sql/branches.sql
@@ -0,0 +1,6 @@
+INSERT INTO llx_societe
+(nom, client, fournisseur, code_client, address, town, phone, email, entity, status, datec)
+VALUES
+('Филиал Заводской', 0, 0, 'BR001', 'Кавказская 4', 'Саратов', '+78452000001', 'zavodskoy@shelter.local', 1, 1, NOW()),
+('Филиал Центральный', 0, 0, 'BR002', 'Мичурина 4', 'Саратов', '+78452000002', 'central@shelter.local', 1, 1, NOW()),
+('Филиал Октябрьский', 0, 0, 'BR003', 'Стрелковая 40', 'Саратов', '+78452000003', 'oktyabrsky@shelter.local', 1, 1, NOW());
